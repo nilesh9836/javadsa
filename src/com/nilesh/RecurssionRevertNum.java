7a7822b04e841d6e0e73a6c8331fd1e91a28ac3b
Nilesh Mishra <nilesh.mishra@lge.com>
2023-06-29 19:01:41 +0530

    javadsa 28june2023

--- src/com/nilesh/RecurssionRevertNum.java
+++ src/com/nilesh/RecurssionRevertNum.java
@@ -4,6 +4,7 @@ public class RecurssionRevertNum {
     public static void main(String[] args) {
         System.out.println(reverse(1020));
         System.out.println(reverseUsingRec(123));
+        System.out.println(rev2(123));
     }
 
     static int reverse(int n) {
@@ -22,5 +23,16 @@ public class RecurssionRevertNum {
             reverseUsingRec(n);
         }
         return rev;
+    }
+
+    static int rev2(int n) {
+        int digit = (int)Math.log10(n) +1;
+        return helper(n,digit);
+    }
+
+    static int helper(int n, int digit) {
+        if(n%10 == n) return n;
+        int rem  = n%10;
+        return rem*(int)Math.pow(10,digit-1) + rev2(n/10);
     }
 }
