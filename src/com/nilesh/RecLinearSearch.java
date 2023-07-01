93d55d9396ef0195ff5d0f88596b711bb8e066ad
Nilesh Mishra <mishranileshkumar6@gmail.com>
2023-07-01 22:15:03 +0530

    javadsa 01jul2023

--- src/com/nilesh/RecLinearSearch.java
+++ src/com/nilesh/RecLinearSearch.java
@@ -3,12 +3,13 @@ package com.nilesh;
 public class RecLinearSearch {
     public static void main(String[] args) {
         int[] arr = new int[]{3,2,1,8,9};
-        System.out.println(linearSearch(arr,9));
+        System.out.println(linearSearch(arr,10));
     }
     static int linearSearch(int[] arr, int t) {
         return helper(arr,t,0);
     }
     static int helper(int[] arr,int t ,int c)  {
+        if(c == arr.length-1) return -1;
         if(arr[c] == t) return c;
         return helper(arr,t,++c);
     }
